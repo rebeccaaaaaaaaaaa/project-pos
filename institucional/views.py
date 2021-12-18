e4cd5bb64ae2576c784cd4e4d318d4d5fdf65fd8
rebeccaaaaaaaaaaa <rebeccaignacio@outlook.com>
2021-12-18 20:42:56 -0300

    listagem doas posts na pg de blog

--- institucional/views.py
+++ institucional/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-
+from blog.models import BlogItem
+from django.views.generic import ListView, DetailView
 
 def index(request):
     return render(request, 'index.html')
@@ -11,8 +12,27 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 
-def blog(request):
-    return render(request, 'blog.html')
-
 def team(request):
     return render(request, 'team.html')
+
+def blog(request):
+    post_list = BlogItem.objects.all()
+    return render(request, 'blog.html', context={'post_list': post_list})
+
+
+class BlogView(ListView):
+    model = BlogItem
+    template_name = 'blog.html'
+    context_object_name = 'post_list'
+    paginate_by = 5
+    
+    def get_context_data(self, **kwargs):
+        context = super(BlogView, self).get_context_data(**kwargs)
+        context['title'] = 'Blog'
+        paginators = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+        pagination_data = self.pagination_data(paginators, page, is_paginated)
+        context.update(pagination_data)
+        return context
+    
